f248ce61bc167d1bc950f53eb4baaff954d04161
Rodrigo Rivero <RodrigoRivero@users.noreply.github.com>
2017-09-21 12:56:48 -1000

    Continuing work

--- dinamicaPrep.R
+++ dinamicaPrep.R
@@ -8,27 +8,40 @@ library(rasterVis)
 library(rgdal) 
 
 # Extent wanted for all from the bigger files
-extentWanted <- extent(-61.7844, -56.35492, -27.12773, -11.59093)
+extentWanted <- extent(-65, -58, -25, -10)
 
 # Changing the working directory to dropbox
 setwd("~/Dropbox/SESYNC GIS Database") 
-initialImage <- raster("Admin/final_level1.tif") # Loading a tif file 
-plot(initialImage) # Plotting the file
-initialImage # Seeing the image properties
 
+# The layers needed are: admin level 2, TNC ecoregions, Brazil Biomes, temperature, precipitation, elevation, population, protected areas, distance to towns, distance to ports, distance to roads, distance to rivers
+# We will need to load all of these 
 
+initialAdmin2 <- raster(“filepath”) 
+initialEcoregions <- raster(“filepath”) 
+initialBrazilBiomes <- raster(“filepath”) 
+initialTemperature <- raster(“filepath”) 
+initialPrecipitation <- raster(“filepath”)
+initialElevation <- raster(“filepath”) 
+initialPopulation <- raster(“filepath”) 
+initialProtectedAreas <- raster(“filepath”) 
+initialTowns <- raster(“filepath”) 
+initialPorts <- raster(“filepath”) 
+initialRoads <- raster(“filepath”) 
+initialRivers <- raster(“filepath”) 
 
+# Then a new image is created with the extent, as well as the projection(the projection can be found in the properties of the initial images)
+mapWanted<- raster(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0", ext = extentWanted)
 
-
-
-
-
-extentForCrop <- extent(-65, -58, -25, -10)
-# I will then crop this out of the 2 maps I am going to use
-initial2012Image <- raster("Land_Use_Class/final_2012.tif") # Loading a tif file 
-initial2013Image <- raster("Land_Use_Class/final_2013.tif") # Loading a tif file 
-
-# Then a new image is created with the extent, as well as the projection(we know it because of the properties of the initial image)
-newMap<- raster(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0", ext = extentWanted)
-#Finally, we crop the section we want from the initial map
-finalMap <- crop(initialImage,newMap)
+# We then crop the initial maps with the map we generated
+finalAdmin2 <- crop(initialAdmin2, mapWanted) 
+finalEcoregions <- crop(initialEcoregions, mapWanted) 
+finalBrazilBiomes <- crop(initialBrazilBiomes, mapWanted) 
+finalTemperature <- crop(initialTemperature, mapWanted) 
+finalPrecipitation <- crop(initialPrecipitation, mapWanted)
+finalElevation <- crop(initialElevation, mapWanted) 
+finalPopulation <- crop(initialPopulation, mapWanted) 
+finalProtectedAreas <- crop(initialProtectedAreas, mapWanted) 
+finalTowns <- crop(initialTowns, mapWanted) 
+finalPorts <- crop(initialPorts, mapWanted) 
+finalRoads <- crop(initialRoads, mapWanted) 
+finalRivers <- crop(initialRivers, mapWanted) 
