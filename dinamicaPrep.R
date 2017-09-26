5757c0d53c12e7328677af7eecfac9aa749ed59f
Rodrigo Rivero <rodrigoriverocastro@gmail.com>
2017-09-25 16:22:58 -1000

    Added Slope as one of the maps needed

--- dinamicaPrep.R
+++ dinamicaPrep.R
@@ -3,7 +3,7 @@
 # This file simply just uses those ideas and works with the many files we need
 
 # Libraries used
-library(raster)
+library(raster) 
 library(rasterVis)
 library(rgdal) 
 
@@ -14,15 +14,16 @@ extentWanted <- extent(-65, -58, -25, -10)
 # Windows 
 # setwd("C:/Users/rodri/Dropbox/SESYNC GIS Database/")
 # Mac
-# setwd("~/Dropbox/SESYNC GIS Database/") 
+setwd("~/Dropbox/SESYNC GIS Database/") 
 # The layers needed are: admin level 2, TNC ecoregions, Brazil Biomes, temperature, precipitation, elevation, population, protected areas, distance to towns, distance to ports, distance to roads, distance to rivers
 
 # We will need to load all of these 
-initialAdmin2 <- raster("Admin/final_level2.tif") 
+initialAdmin2 <- raster("Admin/final_level2.tif")
 initialEcoregions <- raster("Biomes/final_tnc_terr_ecoregions.tif")
 initialBrazilBiomes <- raster("Biomes/final_Brazil_Biomes.tif")
 initialTemperature <- raster("BiophysicalData/Temp_Precipitation/final_air.mon.ltm.v401.tif")
 initialPrecipitation <- raster("BiophysicalData/Temp_Precipitation/final_precip.mon.ltm.v401.tif")
+initialSlope <- raster("BiophysicalData/SRTMv4.1/final_Slope.tif")
 initialElevation <- raster("BiophysicalData/SRTMv4.1/final_Elevation.tif")
 initialPopulation <- raster("Population/final-v4-population-density_2010.tif")
 initialProtectedAreas <- raster("Protected_Area/WDPA_Protected/final_WDPA_Sept2016-shapefile-polygons.tif")
@@ -40,6 +41,7 @@ finalEcoregions <- crop(initialEcoregions, mapWanted)
 finalBrazilBiomes <- crop(initialBrazilBiomes, mapWanted)
 finalTemperature <- crop(initialTemperature, mapWanted)
 finalPrecipitation <- crop(initialPrecipitation, mapWanted)
+finalSlope <- crop(initialSlope, mapWanted)
 finalElevation <- crop(initialElevation, mapWanted)
 finalPopulation <- crop(initialPopulation, mapWanted)
 finalProtectedAreas <- crop(initialProtectedAreas, mapWanted)
@@ -54,10 +56,11 @@ writeRaster(finalEcoregions, filename = "dinamicaMaps/finalEcoregions.tif", data
 writeRaster(finalBrazilBiomes, filename = "dinamicaMaps/finalBrazilBiomes.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalTemperature, filename = "dinamicaMaps/finalTemperature.tif", datatype = "FLT4S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalPrecipitation, filename = "dinamicaMaps/finalPrecipitation.tif", datatype = "FLT4S", format = "GTiff",overwrite=TRUE)
+writeRaster(finalSlope, filename = "dinamicaMaps/finalSlope.tif", datatype = "FLT4S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalElevation, filename = "dinamicaMaps/finalElevation.tif", datatype = "FLT4S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalPopulation, filename = "dinamicaMaps/finalPopulation.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalProtectedAreas, filename = "dinamicaMaps/finalProtectedAreas.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalTowns, filename = "dinamicaMaps/finalTowns.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalPorts, filename = "dinamicaMaps/finalPorts.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
-writeRaster(finalRoads, filename = "dinamicaMaps/Temporary/finalRoads.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
+writeRaster(finalRoads, filename = "dinamicaMaps/finalRoads.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
 writeRaster(finalRivers, filename = "dinamicaMaps/finalRivers.tif", datatype = "INT2S", format = "GTiff",overwrite=TRUE)
