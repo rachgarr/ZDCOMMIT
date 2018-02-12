b8c960c5d2b9568c3ba15d1e8dc88c1dd03f0f95
Rodrigo Rivero <rodrigoriverocastro@gmail.com>
2018-02-12 12:26:20 -1000

    Made better by keeping it as a template for multiple uses

--- featureToRaster.py
+++ featureToRaster.py
@@ -1,49 +1,32 @@
-# By: Rodrigo River
+# By: Rodrigo Rivero
 # This file is meant to be used with ArcMap in order to extract information from a shapefile and export it into a raster
-
-# The main file we have is 2012PanelData.shp and what we want to get out is the following
-# soy_area
-# corn_area
-# soy_yield
-# cattle_heads
-
-# Import system modules
+# Import system modules needed
 import arcpy
 from arcpy import env
 from arcpy.sa import *
 
-# Set environment settings
 # Workspace(this might change depending on your folder structure)
 env.workspace = "C:/Users/rodri/Desktop/Sandbox"
 
+# Clear environment
 arcpy.ClearEnvironment("snapRaster")
 arcpy.ClearEnvironment("cellSize")
 arcpy.ClearEnvironment("extent")
 
-arcpy.env.snapRaster = Raster("cropped2012.tif")
-arcpy.env.cellSize = Raster("cropped2012.tif")
-arcpy.env.extent = Raster("cropped2012.tif")
+# Environment settings
+templateMap = ""
+arcpy.env.snapRaster = Raster(templateMap)
+arcpy.env.cellSize = Raster(templateMap)
+arcpy.env.extent = Raster(templateMap)
 
-# Set main feature
-inFeature = "2012PanelData.shp"
+# Set main feature(the shapefile you want to create the raster from)
+inFeature = ""
 
-# Soy Area
-outRaster = "soyArea2012.tif"
-field = "soy_area"
+# Name of the new raster
+outRaster = ""
+
+# Name of the field in the shapefile wanted
+field = ""
+
 ## Execute FeatureToRaster
-arcpy.FeatureToRaster_conversion(inFeature, field, outRaster)
-
-# Corn Area
-outRaster = "cornArea2012.tif"
-field = "corn_area"
-arcpy.FeatureToRaster_conversion(inFeature, field, outRaster)
-
-# Soy Yield
-outRaster = "soyYield2012.tif"
-field = "soy_yield"
-arcpy.FeatureToRaster_conversion(inFeature, field, outRaster)
-
-# Cattle Heads
-outRaster = "cattleHeads2012.tif"
-field = "cattle_hea"
 arcpy.FeatureToRaster_conversion(inFeature, field, outRaster)
